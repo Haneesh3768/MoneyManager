4a37879092c27d3bfd3fd5d8df0f6c46b615a6df
Haneesh3768 <149151573+Haneesh3768@users.noreply.github.com>
2025-10-06 23:40:53 +0530

    first commit

--- src/main/java/com/project/expensetracker/controller/CategoryController.java
+++ src/main/java/com/project/expensetracker/controller/CategoryController.java
@@ -1,7 +1,11 @@
 package com.project.expensetracker.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +27,17 @@ public class CategoryController {
 	public ResponseEntity<CategoryDTO> saveCategory(@RequestBody CategoryDTO categoryDTO){
 		CategoryDTO savedCategory = categoryService.savCategory(categoryDTO);
 		return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CategoryDTO>> getCategories(){
+		List<CategoryDTO> categories= categoryService.getCategoriesForCurrentUser();
+		return ResponseEntity.ok(categories);
+	}
+	
+	@GetMapping("/{type}")
+	public ResponseEntity<List<CategoryDTO>> getCategoriesByTypeForCurrentUser(@PathVariable String type){
+		List<CategoryDTO> list = categoryService.getCategoriesByTypeForCurrentUser(type);
+		return ResponseEntity.ok(list);
 	}
 }
--- src/main/java/com/project/expensetracker/service/CategoryService.java
+++ src/main/java/com/project/expensetracker/service/CategoryService.java
@@ -1,5 +1,7 @@
 package com.project.expensetracker.service;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -24,13 +26,27 @@ public class CategoryService {
 	public CategoryDTO savCategory(CategoryDTO categoryDTO) {
 		ProfileEntity profile = profileService.getCurrentProfile();
 		if(categoryRepository.existsByNameAndProfileId(categoryDTO.getName(),profile.getId() )) {
-			throw new ResponseStatusException(HttpStatus.CONFLICT,"Category with this name already there");
+			throw new RuntimeException("Category with this name already exists");
 		}
 		CategoryEntity newCategory = toEntity(categoryDTO, profile);
 		newCategory = categoryRepository.save(newCategory);
 		return toDTO(newCategory);
 	}
 	
+	
+	//get categories for current user 
+	public List<CategoryDTO> getCategoriesForCurrentUser(){
+		ProfileEntity profile = profileService.getCurrentProfile();
+		List<CategoryEntity> categories = categoryRepository.findByProfileId(profile.getId());
+		return categories.stream().map(this::toDTO).toList();
+	}
+	
+	//get categories by type for current user
+	public List<CategoryDTO> getCategoriesByTypeForCurrentUser(String type){
+		ProfileEntity profile = profileService.getCurrentProfile();
+		List<CategoryEntity> entities = categoryRepository.findByTypeAndProfileId(type, profile.getId());
+		return entities.stream().map(this::toDTO).toList();
+	}
 	
 	//helper methods
 	private CategoryEntity toEntity(CategoryDTO categoryDTO, ProfileEntity profile) {
