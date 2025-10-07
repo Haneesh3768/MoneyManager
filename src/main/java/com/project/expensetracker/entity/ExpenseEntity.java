ec1fcecb5d5ca6095c76ffca9299ba170210fed2
Haneesh3768 <149151573+Haneesh3768@users.noreply.github.com>
2025-10-07 22:36:34 +0530

    first commit

---
+++ src/main/java/com/project/expensetracker/entity/ExpenseEntity.java
@@ -0,0 +1,55 @@
+package com.project.expensetracker.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "expense_table")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ExpenseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String icon;
+	private LocalDate date;
+	private BigDecimal amount;
+	@Column(updatable = false)
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "category_	id" , nullable = false)
+	private CategoryEntity category;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "profile_id" , nullable = false)
+	private ProfileEntity profile;
+	
+	public void prePersist() {
+		if(this.date==null) {
+			this.date = LocalDate.now();
+		}
+	}
+}
