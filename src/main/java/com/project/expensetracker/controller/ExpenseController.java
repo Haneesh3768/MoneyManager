310891c1145d95d0573cd8b9928cfd2dececaa7f
Haneesh3768 <149151573+Haneesh3768@users.noreply.github.com>
2025-10-08 23:11:41 +0530

    first commit

---
+++ src/main/java/com/project/expensetracker/controller/ExpenseController.java
@@ -0,0 +1,27 @@
+package com.project.expensetracker.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.expensetracker.dto.ExpenseDTO;
+import com.project.expensetracker.service.ExpenseService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/expenses")
+public class ExpenseController {
+
+	private final ExpenseService expenseService;
+	
+	@PostMapping
+	public ResponseEntity<ExpenseDTO> addExpense(@RequestBody ExpenseDTO dto){
+		ExpenseDTO saved = expenseService.addExpense(dto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+	}
+}
